8e6ff0b59feb77f511a509c7b6476fac78998b5b
mikkosaarisalo <mikko.saarisalo@gmail.com>
2022-01-27 09:46:50 +0200

    Fix pretrained model download directory
    Change pretrained download directory from 'checkpoints' to 'checkpoints-pretrained'.  The result matches with the example scripts.

--- scripts/download_checkpoints.sh
+++ scripts/download_checkpoints.sh
@@ -1,3 +1,3 @@
-cd checkpoints
+cd checkpoints-pretrained
 wget https://cvg-data.inf.ethz.ch/cross-descriptor-vis-loc-map-ICCV2021/checkpoints/model.pth
 wget https://cvg-data.inf.ethz.ch/cross-descriptor-vis-loc-map-ICCV2021/checkpoints/model-cvsift.pth
